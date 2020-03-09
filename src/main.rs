6702048a847f43ae5aec091f9df128ca177df6ca
Darshan Parajuli <parajulidarshan@gmail.com>
2020-03-08 19:16:27 -0700

    Support basic cmdline args

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,28 @@
+use clap;
 use image;
+
+use clap::{App, Arg};
 use image::{GenericImageView, ImageBuffer, Rgba, RgbaImage};
-use std::env;
 use std::process;
 
-fn main() {
-    let file_names: Vec<String> = env::args().skip(1).collect();
-    if file_names.len() < 2 {
-        exit_error("Usage: imgrow <file1> <file2> ..".into());
-    }
+const DEFAULT_SPACING: &'static str = "10";
 
-    let spacing = 10;
+fn main() {
+    let config = get_config();
+
+    let spacing = config.spacing;
     let mut total_width = 0;
     let mut max_height = 0;
     let mut images = Vec::new();
 
-    for (i, name) in file_names.iter().enumerate() {
+    for (i, name) in config.input.iter().enumerate() {
         match image::open(name) {
             Ok(img) => {
                 let (w, h) = img.dimensions();
                 max_height = std::cmp::max(h, max_height);
                 total_width += w;
 
-                if i < file_names.len() - 1 {
+                if i < config.input.len() - 1 {
                     total_width += spacing;
                 }
 
@@ -33,8 +34,8 @@ fn main() {
         }
     }
 
-    println!("total width: {}", total_width);
-    println!("max height: {}", max_height);
+    println!("Width: {}", total_width);
+    println!("Height: {}", max_height);
 
     let mut buffer: RgbaImage = ImageBuffer::new(total_width, max_height);
     for x in 0..total_width {
@@ -61,13 +62,72 @@ fn main() {
         }
     }
 
-    match buffer.save("output.png") {
+    match buffer.save(&config.output) {
         Ok(_) => {
-            println!("Saved to output.png");
+            println!("Saved to '{}'.", config.output);
         }
         Err(e) => {
-            exit_error(format!("Error saving to output.png: {}", e));
+            exit_error(format!("Error saving to '{}': {}", config.output, e));
         }
+    }
+}
+
+struct Config {
+    input: Vec<String>,
+    output: String,
+    spacing: u32,
+}
+
+fn get_config() -> Config {
+    let matches = App::new("imgrow")
+        .version("0.1.0")
+        .author("Darshan Parajuli <parajulidarshan@gmail.com>")
+        .about("Put a bunch of images in a row.")
+        .arg(
+            Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .value_name("FILE")
+                .help("Sets output file.")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("spacing")
+                .short("s")
+                .long("spacing")
+                .help("Number of pixels in between images.")
+                .required(false)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("images")
+                .long("images")
+                .short("i")
+                .value_name("IMAGE")
+                .multiple(true)
+                .required(true)
+                .help("Images."),
+        )
+        .get_matches();
+
+    let spacing = matches
+        .value_of("spacing")
+        .unwrap_or(DEFAULT_SPACING)
+        .parse::<u32>();
+    if spacing.is_err() {
+        exit_error("Invalid spacing value!".into());
+    }
+    let spacing = spacing.unwrap();
+
+    Config {
+        input: matches
+            .values_of("images")
+            .unwrap()
+            .map(|s| s.to_string())
+            .collect(),
+        output: matches.value_of("output").unwrap().to_string(),
+        spacing: spacing,
     }
 }
 
